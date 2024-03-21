4ef748d0afef5c915f58eb2663dd6cb8f0d68159
Amol_Narayan_Dhumal <amoldhumal95@gmail.com>
2024-03-22 03:05:48 +0530

    Update master.js

--- js/master.js
+++ js/master.js
@@ -24,7 +24,7 @@ function createBlob(){
 
     document.getElementById("blob").style.cssText = `border-radius: ${borderRadius};height: ${blobHeight}px; width:${blobWidth}px;`;
 
-    outputCode.value = `border-radius:${borderRadius}`;
+    outputCode.value = `border-radius:${borderRadius}` + ";";
 }
 
 document.getElementById("copy").addEventListener("click",function(){
